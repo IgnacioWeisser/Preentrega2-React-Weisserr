7b298e6e9806af1aa2abeacb320cafe0dc1a5b66
Ignacio Weisser <weisser.89@gmail.com>
2024-06-10 00:10:41 -0400

    Segunda entrega

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
-
-import NavBarComponent from './components/NavBarComponent/NavBarComponent';
-import ItemListContainerComponent from './components/ItemListContainerComponent/ItemListContainerComponent';
+import MainRouter from "./routes/MainRouter";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 function App() {
   return (
     <>
-      <NavBarComponent />
-      <ItemListContainerComponent greeting="Bienvenidos a la tienda de computadores del sur del mundo Weisser Pc Store" />
+      <MainRouter />
     </>
   );
 }
